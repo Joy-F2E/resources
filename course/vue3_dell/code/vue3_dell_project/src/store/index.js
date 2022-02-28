b88da64372c31b4cc76a35fd5d28a0c6cc4eae80
Joy <799840822@qq.com>
2022-02-28 14:21:53 +0800

    add new demo

--- course/vue3_dell/code/vue3_dell_project/src/store/index.js
+++ course/vue3_dell/code/vue3_dell_project/src/store/index.js
@@ -1,10 +1,32 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
     name: "Joy",
+    userInfo: "",
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    // 主要进行同步操作，不允许写异步代码
+    changeUserName(state, value) {
+      state.name = value;
+    },
+  },
+  actions: {
+    // 主要进行异步操作
+    getUserInfo({ state, commit }) {
+      axios
+        .get(
+          "https://www.fastmock.site/mock/786e34a2cfe42b4b4e85a42e8367f7eb/vue/api/getUserInfo"
+        )
+        .then((res) => {
+          if (res.data.code === 200) {
+            state.userInfo = res.data.data;
+            console.log("state.userInfo: ", res.data.data);
+          }
+        })
+        .catch((err) => {});
+    },
+  },
   modules: {},
 });
--- course/vue3_dell/code/vue3_dell_project/src/views/Login.vue
+++ course/vue3_dell/code/vue3_dell_project/src/views/Login.vue
@@ -2,18 +2,44 @@
   <div class="about">
     <h1>This is an login page</h1>
     <h3>User: {{ userName }}</h3>
+    <p>{{ userInfo }}</p>
+    <button @click="handleBtnClick">Change Name</button>
+    <button @click="handleUserBtnClick">getUserInfo</button>
   </div>
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { toRefs, computed } from "vue";
+// 用户名称
+const uerNameReleativeEffect = () => {
+  const store = useStore();
+  const { name: userName } = toRefs(store.state);
+  const handleBtnClick = () => {
+    store.commit("changeUserName", "Ejy");
+  };
+  return { userName, handleBtnClick };
+};
+
+// 用户信息
+const userInfoRelativeEffect = () => {
+  const store = useStore();
+  const userInfo = computed(() => store.state.userInfo);
+  const handleUserBtnClick = async () => {
+    await store.dispatch("getUserInfo");
+  };
+  return { userInfo, handleUserBtnClick };
+};
 export default {
-  data() {
-    return {};
-  },
-  computed: {
-    userName() {
-      return this.$store.state.name;
-    },
+  setup() {
+    const { userName, handleBtnClick } = uerNameReleativeEffect();
+    const { userInfo, handleUserBtnClick } = userInfoRelativeEffect();
+    return {
+      userName,
+      handleBtnClick,
+      userInfo,
+      handleUserBtnClick,
+    };
   },
 };
 </script>
